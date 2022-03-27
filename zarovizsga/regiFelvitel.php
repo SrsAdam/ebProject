ce83a7dc5a7bc9f1eee6939338fa7c2eca6299d2
DLE5450\admin <llissa74@gmail.com>
2022-03-27 17:17:34 +0200

    megerősítő email box tartalom ellenörzés és jelszó titkosítás

--- zarovizsga/regiFelvitel.php
+++ zarovizsga/regiFelvitel.php
@@ -8,22 +8,33 @@ $conn = mysqli_connect("localhost", "root", "","elso") or die("Csatlakozási hib
 $v_NAME = $_POST['NAME'];
 $v_USERNAME = $_POST['USERNAME'];
 $v_EMAIL = $_POST['EMAIL'];
-$v_PASSWORD = $_POST['PASSWORD'];
+$v_PASSWD = $_POST['PASSWD'];
+$v_PASSWD2 = $_POST['PASSWD2'];
 $v_ADOSZAM =$_POST['ADOSZAM'];
 $v_SZLASZAM=$_POST['SZLASZAM'];
 $v_WEBLINK = $_POST['WEBLINK'];
 $v_MEGYE = $_POST['MEGYE'];
 
-if ( isset($v_NAME) && isset($v_USERNAME) && isset($v_EMAIL) && isset($v_PASSWORD) && isset($v_ADOSZAM) && isset($v_SZLASZAM) && isset($v_WEBLINK) && isset($v_MEGYE))
+if ($v_PASSWD !=$v_PASSWD2 )
+{
+    
+    die("A két jelszó nem egyezik!");
+    
+}
+else
+{
+if ( isset($v_NAME) && isset($v_USERNAME) && isset($v_EMAIL) && isset($v_PASSWD) && isset($v_ADOSZAM) && isset($v_SZLASZAM) && isset($v_WEBLINK) && isset($v_MEGYE))
  
- 
+// ellenőrzéshez: print ($v_PASSWD);
 
- $stmt = mysqli_prepare($conn,"INSERT INTO regisztracio(NAME, USERNAME, EMAIL, PASSWORD, ADOSZAM, SZLASZAM, WEBLINK,MEGYE) VALUES ( ?, ?, ?, ?, ?,?,?,?)");
- mysqli_stmt_bind_param($stmt,"ssssiiss",$v_NAME, $v_USERNAME, $v_EMAIL, $v_PASSWORD,$v_ADOSZAM, $v_SZLASZAM,$v_WEBLINK,$v_MEGYE);
+$v_PASSWD = md5($v_PASSWD);
+
+ $stmt = mysqli_prepare($conn,"INSERT INTO regisztracio(NAME, USERNAME, EMAIL, PASSWD, ADOSZAM, SZLASZAM, WEBLINK,MEGYE) VALUES ( ?, ?, ?, ?, ?,?,?,?)");
+ mysqli_stmt_bind_param($stmt,"ssssiiss",$v_NAME, $v_USERNAME, $v_EMAIL, $v_PASSWD,$v_ADOSZAM, $v_SZLASZAM,$v_WEBLINK,$v_MEGYE);
  
  $sikeres = mysqli_stmt_execute($stmt);
-
+// ellenőrzéshez: print ($v_PASSWD);
 print("Sikeres regisztráció!");	
-
+}
 
 ?>
