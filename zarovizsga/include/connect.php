fab02ca7339de716bda9d952c8862980c5728f64
DESKTOP-UFBFKHE\Sr-71 <edem.sarosi@gmail.com>
2022-03-28 17:58:16 +0200

    Teszt!!!

--- zarovizsga/include/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "","elso") or die("Csatlakozási hiba");
-
-?>
--- zarovizsga/kettö_keresös_fajlok/inc_keresesOsszes.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset ($_POST['k_submit']) AND $_POST['k_nem']) {
-    $kutyakora=$_POST['k_kor'];
-    $kutyaneme=$_POST['k_nem'];
-    header("location: ../alap/keresesOsszes.php?kora=$kutyakora&&neme=$kutyaneme");
-} 
-
-
