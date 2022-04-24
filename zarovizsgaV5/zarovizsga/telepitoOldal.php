58227779fcfee70bb1e375d85d7107284828a73a
DESKTOP-UFBFKHE\Sr-71 <edem.sarosi@gmail.com>
2022-04-24 19:00:25 +0200

    Találatok/kedvencek formázása nem sikerült!!! Minden más működik

---
+++ zarovizsgaV5/zarovizsga/telepitoOldal.php
@@ -0,0 +1,8 @@
+<?php
+
+$conn=mysqli_connect("localhost","root","","elso")or die("Csatlakozási hiba!");
+echo ("Töltse le az alkalmazást!");?> 
+<a href="https://www.tutorialrepublic.com/php-tutorial/php-switch-case-statements.php" target=\"_blank\" class="btn">Asztali alkalmazás telepítő oldal</a>
+<?php
+
+ 
